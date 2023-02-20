046c2fbf51725670b43372c8cc927248996680e6
mdriyadmr968 <mdriyadmr968@gmail.com>
2023-02-20 20:29:07 +0600

    trying head api

--- src/pages/post/[id].js
+++ src/pages/post/[id].js
@@ -1,6 +1,5 @@
 import { Link } from "gatsby";
 import React, { useEffect, useState } from "react";
-import { Helmet } from "react-helmet";
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
@@ -29,51 +28,6 @@ const Singlepost = (props) => {
 
   return (
     <div style={{ width: "60%", margin: "auto", paddingTop: "2%" }}>
-      <Helmet>
-        <meta content="article" property="og:type" />
-        <meta
-          property="og:title"
-          content="Fact Check: Did Sourav Ganguly actually resign as the BCCI President? Rumours abound;"
-        />
-        <meta
-          property="og:url"
-          content="https://facthunt.in/posts/2135/Fact-Check:-Did-Sourav-Ganguly-actually-resign-as-the-BCCI-President?-Rumours-abound;"
-        />
-        <meta
-          property="og:description"
-          content="Sourav Ganguly&amp;#39;s Tweet about him &amp;quot;planning to start something&amp;quot; triggered rumours that Ganguly is going to quit as the President of BCCI. In a clarifying statement, Jay Shah put an end to such rumours.
-"
-        />
-        <meta
-          property="og:image"
-          content="https://www.india.com/wp-content/uploads/2021/12/Sourav-Ganguly-PTI.jpg"
-        />
-        <meta
-          name="description"
-          content="Sourav Ganguly&amp;#39;s Tweet about him &amp;quot;planning to start something&amp;quot; triggered rumours that Ganguly is going to quit as the President of BCCI. In a clarifying statement, Jay Shah put an end to such rumours."
-        />
-        <meta name="keywords" />
-        <meta content="text/html; charset=UTF-8" http-equiv="Content-Type" />
-
-        <meta content="summary_large_image" name="twitter:card" />
-        <meta content="@facthunt_in" name="twitter:site" />
-        <meta content="@facthunt_in" name="twitter:creator" />
-        <meta
-          name="twitter:title"
-          content="Fact Check: Did Sourav Ganguly actually resign as the BCCI President? Rumours abound;"
-        />
-        <meta
-          name="twitter:description"
-          content="Sourav Ganguly&amp;#39;s Tweet about him &amp;quot;planning to start something&amp;quot; triggered rumours that Ganguly is going to quit as the President of BCCI. In a clarifying statement, Jay Shah put an end to such rumours.
-"
-        />
-        <meta
-          name="twitter:image"
-          content="https://www.india.com/wp-content/uploads/2021/12/Sourav-Ganguly-PTI.jpg"
-        />
-
-        <meta content="1667939569986269" property="fb:pages" />
-      </Helmet>
       <SkeletonTheme baseColor="#202020" highlightColor="#444">
         {loading ? (
           <Skeleton count={50} />
@@ -100,8 +54,24 @@ const Singlepost = (props) => {
           </div>
         )}
       </SkeletonTheme>
+      <Head post={post} />
     </div>
   );
 };
+
+const Head = ({ post }) => (
+  <>
+    <title>{post?.title}</title>
+    <meta property="og:title" content={post?.title} />
+    <meta property="og:category" content={post?.category} />
+    <meta
+      property="og:image"
+      content="https://i.ibb.co/x7JmZNX/download-300x300.jpg"
+      alt="download-300x300"
+    />
+
+    <meta property="og:url" content="https://gatsby-blog-ashy.vercel.app/" />
+  </>
+);
 
 export default Singlepost;
