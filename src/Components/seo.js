e39927e319333f353add510b62b61dbb0651cbf9
mdriyadmr968 <mdriyadmr968@gmail.com>
2023-02-21 17:52:17 +0600

    test title

--- src/Components/seo.js
+++ src/Components/seo.js
@@ -4,7 +4,7 @@ export const SEO = ({ title }) => {
   return (
     <>
       <title>{title || "hello title"}</title>
-      <meta name="title" content="testing default title" />
+      <meta name="title" content={title} />
       <meta name="Description" content="testing default Description" />
     </>
   );
