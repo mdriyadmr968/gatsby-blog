b9cc0110ec596c3d5255bd06d85c9ec17c212f95
mdriyadmr968 <mdriyadmr968@gmail.com>
2023-02-21 16:19:35 +0600

    only title preview

--- src/Components/seo.js
+++ src/Components/seo.js
@@ -1,32 +1,21 @@
 import React from "react";
-import { useSiteMetadata } from "../Hooks/use-site-metadata";
+// import { useSiteMetadata } from "../Hooks/use-site-metadata";
 
-export const SEO = ({ title, description, pathname, children }) => {
-  const {
-    title: defaultTitle,
-    description: defaultDescription,
-    image,
-    siteUrl,
-  } = useSiteMetadata();
+export const SEO = ({ title }) => {
+  //   const {
+  //     title: defaultTitle,
+  //     description: defaultDescription,
+  //     image,
+  //     siteUrl,
+  //   } = useSiteMetadata();
 
   const seo = {
-    title: title || defaultTitle,
-    description: description || defaultDescription,
-    image: `${siteUrl}${image}`,
-    url: `${siteUrl}${pathname || ``}`,
+    title: title,
   };
 
   return (
     <>
       <title>{seo.title}</title>
-      <meta name="description" content={seo.description} />
-      <meta name="image" content={seo.image} />
-      <link
-        rel="shortcut icon"
-        href="/corbin-cc-logo2.png"
-        type="image/x-icon"
-      />
-      {children}
     </>
   );
 };
