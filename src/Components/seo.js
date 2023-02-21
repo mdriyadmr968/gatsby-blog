6027e48c61d75124987384b7f30554fae22f4da7
mdriyadmr968 <mdriyadmr968@gmail.com>
2023-02-21 16:23:25 +0600

    move head to post

--- src/Components/seo.js
+++ src/Components/seo.js
@@ -2,12 +2,6 @@ import React from "react";
 // import { useSiteMetadata } from "../Hooks/use-site-metadata";
 
 export const SEO = ({ title }) => {
-  //   const {
-  //     title: defaultTitle,
-  //     description: defaultDescription,
-  //     image,
-  //     siteUrl,
-  //   } = useSiteMetadata();
 
   const seo = {
     title: title,
--- src/pages/index.js
+++ src/pages/index.js
@@ -77,4 +77,4 @@ const IndexPage = () => {
 
 export default IndexPage;
 
-export const Head = () => <SEO title="my gatsby blog" />;
+// export const Head = () => <SEO title="my gatsby blog" />;
